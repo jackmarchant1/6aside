07e475e27e753c0ad241e2875b92e0714308364d
Jack Marchant <jackmarchantt@gmail.com>
2023-10-19 19:48:51 +0100

    Animations added

--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -3,11 +3,22 @@ import axios from 'axios';
 import PlayerDisplay from './components/PlayerDisplay';
 import { Player } from './lib/objects/Classes';
 import Papa from 'papaparse';
+import { useTrail, animated } from 'react-spring';
 
 
 function DataDisplay() {
   const [squadData, setSquadData] = useState();
   const [fanData, setFanData] = useState();
+  const [isModalVisible, setIsModalVisible] = useState(false); // New state for modal visibility
+
+  const [trail, setTrail] = useTrail(squadData?.length || 0, () => ({
+    transform: 'translateX(0%)',
+    opacity: 1,
+  }));
+  const toggleModal = () => {
+    setIsModalVisible(prevState => !prevState);
+  };
+
 
   useEffect(() => {
     const squadURL = "https://docs.google.com/spreadsheets/d/1x6YQo3FwHJOeLdMk9Annqn1vWdacNyhVrZ2Lj2oygzQ/export?format=csv&id=1x6YQo3FwHJOeLdMk9Annqn1vWdacNyhVrZ2Lj2oygzQ&gid=0";
@@ -43,8 +54,22 @@ function DataDisplay() {
             .catch(error => {
                 console.error("There was an error fetching the data", error);
             });
-
   }, []);
+    useEffect(() => {
+        if (isModalVisible) {
+        setTrail({
+            transform: 'translateX(0%)',
+            opacity: 1,
+            from: { transform: 'translateX(50%)', opacity: 1 },
+        });
+        } else {
+        setTrail({
+            transform: 'translateX(0%)',
+            opacity: 1,
+            from: {transform: 'translateX(-20%)'},
+        });
+        }
+    }, [isModalVisible]);
 
   return (
     <>
@@ -54,25 +79,25 @@ function DataDisplay() {
         <p>Trauma FC, 25th, Pitch 1</p>
     </div>
 
+
     <div className="squad">
         <div className="d-flex justify-content-between p-2 align-items-center">
             <h1 clasName="title">Squad</h1>
-            <i className="bi bi-sort-numeric-down title"></i>
+            <i className="bi bi-sort-numeric-down title" onClick={toggleModal}></i>
         </div>
         
-        <div className="d-flex flex-wrap justify-content-around">
-            {squadData && squadData.map((item, index) => (
-                <div key={index}>
-                    {/* Render your data here */}
-                    <PlayerDisplay {...new Player(item.PlayerID, item.PlayerName, item.PlayerNumber, "")} />
-                </div>
+        <div className={`d-flex ${isModalVisible ? 'flex-column align-items-start' : 'flex-wrap justify-content-around'}`}>
+            {squadData && trail.map((props, index) => (
+            <animated.div key={index} style={props}>
+                <PlayerDisplay {...new Player(squadData[index].PlayerID, squadData[index].PlayerName, squadData[index].PlayerNumber, "")} />
+            </animated.div>
             ))}
         </div>
-    </div>
+    </div> 
 
     <div className="fans">
         <h1 className="px-2 title">Fans</h1>
-        <div className="d-flex flex-wrap justify-content-around">
+        <div className="d-flex flex-wrap justify-content-between">
             {fanData && fanData.map((item, index) => (
                 <div key={index}>
                     {/* Render your data here */}
